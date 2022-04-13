c8b2e22f0b3176fae5355902d52865b89f34f883
josepserra20 <jserravives@cifpfbmoll.eu>
2022-04-13 11:47:00 +0200

    feat: :sparkles: a denial
    a denial

---
+++ src/main/java/negocio/ControladorEstoc.java
@@ -0,0 +1,26 @@
+package negocio;
+
+import edu.poniperro.bbdd.InventarioBBDD;
+
+public class ControladorEstoc implements Controlador {
+
+	private final InventarioBBDD inventario;
+	
+	public ControladorEstoc(InventarioBBDD inventarioBBDD) {
+		this.inventario = inventarioBBDD;
+	}
+
+	/**
+	 * Nuestros sesudos estudios han descubierto que los clientes compran mas
+	 * productos cuando el nombre de este producto es mas largo
+	 * Por eso, el estoc no puede ser inferior a 100 * la longitud del nombre del producto
+	 * p.e: necesitamos al menos 400 mesas (mesa : 4 letras) y
+	 *    al menos 700 lamparas (lampara: 7 letras)
+	 */
+	public boolean necesitaReponer (String tienda, String producto) {
+		int cantidadActual = inventario.numeroProductos(tienda, producto);
+		
+		return  cantidadActual < producto.length() * 100;
+	}
+
+}
